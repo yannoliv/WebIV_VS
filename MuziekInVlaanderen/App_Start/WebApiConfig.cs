76af5d6e1426cc1f2b904ffaf819d2632df1b4cd
yannoliv <yannoliv@student.arteveldehs.be>
2019-03-14 11:46:59 +0100

    Klaar voor met Angular te beginnen.
    (Set a Default Formatter for ASP.NET Web API 2)
    beginnen aan
    Build SPA application with Angular

--- MuziekInVlaanderen/App_Start/WebApiConfig.cs
+++ MuziekInVlaanderen/App_Start/WebApiConfig.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace MuziekInVlaanderen
 {
@@ -19,6 +21,17 @@ namespace MuziekInVlaanderen
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            // Enable CORS for the Angular App
+            var cors = new EnableCorsAttribute("http://localhost:4200", "*", "*");
+            config.EnableCors(cors);
+
+            // Set JSON formatter as default one and remove XmlFormatter
+            var jsonFormatter = config.Formatters.JsonFormatter;
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+            jsonFormatter.SerializerSettings.DateTimeZoneHandling = Newtonsoft.Json.DateTimeZoneHandling.Utc;
         }
+
     }
 }
