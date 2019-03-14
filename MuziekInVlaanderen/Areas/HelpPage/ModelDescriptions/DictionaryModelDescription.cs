dee5be74542a0ef1f1bd9610056fae2e185108ad
yannoliv <yannoliv@student.arteveldehs.be>
2019-03-14 09:41:55 +0100

    Fresh start

--- MuziekInVlaanderen/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace MuziekInVlaanderen.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- MuziekInVlaanderen/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace MuziekInVlaanderen.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
