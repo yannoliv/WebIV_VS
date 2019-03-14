2495a2c267363ae41536dd30c50fbeba2098fbbf
yannoliv <yannoliv@student.arteveldehs.be>
2019-03-14 10:38:12 +0100

    Domein - Locatie, Evenement, Categorie

---
+++ MuziekInVlaanderen/Models/Domain/Evenement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MuziekInVlaanderen.Models.Domain
+{
+    public class Evenement
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Titel { get; set; }
+
+        [Required]
+        public Categorie Categorie { get; set; }
+
+        [Required]
+        public Locatie Locatie { get; set; }
+
+        [Required]
+        public string Beschrijving { get; set; }
+
+        [Required]
+        public DateTime Datum { get; set; }
+    }
+}
---
+++ MuziekInVlaanderen/Models/Domain/Locatie.cs
@@ -0,0 +1,9 @@
+﻿namespace MuziekInVlaanderen.Models.Domain
+{
+    public class Locatie
+    {
+        public string Straat { get; set; }
+        public string PostCode { get; set; }
+        public string Gemeente { get; set; }
+    }    
+}
