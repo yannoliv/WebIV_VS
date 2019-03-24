9cfbb63a41423940ec1ee45d47031fa421777bdf
yannoliv <yannoliv@student.arteveldehs.be>
2019-03-24 10:26:29 +0100

    Authentication: Step 1 & Step 2

--- MuziekInVlaanderen/Startup.cs
+++ MuziekInVlaanderen/Startup.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -11,10 +13,13 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using MuziekInVlaanderen.Data;
 using MuziekInVlaanderen.Data.Repositories;
 using MuziekInVlaanderen.Models.Domain;
 using MuziekInVlaanderenAPI.Data;
+using NSwag;
+using NSwag.SwaggerGeneration.Processors.Security;
 
 namespace MuziekInVlaanderen
 {
@@ -44,9 +49,27 @@ namespace MuziekInVlaanderen
                 c.Title = "MuziekInVlaanderenAPI";
                 c.Version = "v1";
                 c.Description = "The MuziekInVlaanderenAPI documentation description.";
+                c.DocumentProcessors.Add(new SecurityDefinitionAppender("JWT Token", new SwaggerSecurityScheme {
+                    Type = SwaggerSecuritySchemeType.ApiKey, Name = "Authorization", In = SwaggerSecurityApiKeyLocation.Header,
+                    Description = "Copy 'Bearer' + valid JWT token into field"}));
+                c.OperationProcessors.Add(new OperationSecurityScopeProcessor("JWT Token"));
             });
 
             services.AddCors(options => options.AddPolicy("AllowAllOrigins", builder => builder.AllowAnyOrigin()));
+
+            services.AddAuthentication(x => { x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme; })
+                .AddJwtBearer(x => {
+                    x.RequireHttpsMetadata = false;
+                    x.SaveToken = true;
+                    x.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Tokens:Key"])),
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        RequireExpirationTime = true//Ensure token hasn't expired
+                    };
+            });  
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
@@ -70,6 +93,7 @@ namespace MuziekInVlaanderen
             app.UseSwaggerUi3();
             app.UseSwagger();
             app.UseCors("AllowAllOrigins");
+            app.UseAuthentication();
 
             initializer.InitializeData();
         }
