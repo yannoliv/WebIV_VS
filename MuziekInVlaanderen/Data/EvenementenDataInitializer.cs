85ca0bc5cd18464e9b262fb156b7ddb704221031
yannoliv <yannick.olivier@hotmail.com>
2019-05-11 19:20:39 +0200

    Data gevuld

--- MuziekInVlaanderen/Data/EvenementenDataInitializer.cs
+++ MuziekInVlaanderen/Data/EvenementenDataInitializer.cs
@@ -28,20 +28,25 @@ namespace MuziekInVlaanderenAPI.Data
             _dbContext.Database.EnsureDeleted();
             if (_dbContext.Database.EnsureCreated())
             {
+                //fotograaf
+                var lisa = new Fotograaf("Lisa Oliviero", "profielfotolisa.png", new List<string>() { "twitter.be/lisa23", "facebook.nl/lisaolivero" });
+                var robert = new Fotograaf("Robert Van Straeten", "profielfotorobert.png", new List<string>() { "twitter.be/robert", "facebook.nl/roberto" });
+
 
                 // Evenementen aanmaken 
                 var lijstEvenementen = new List<Evenement>() {
 
-                    new Evenement("Port of Bortdom","Olmenfeesten = spel, feest, dans en eten. Er is de Olmenkwis, een afterwork, kindernamiddag, fietstocht, BBQ, kampvuur, fuif en optreden. En dat allemaal in de mooiste groene oase van Sint-Niklaas. Alle info en prijzen via www.olmenfeesten.be",
+                    new Evenement("Olmenfeesten","Olmenfeesten = spel, feest, dans en eten. Er is de Olmenkwis, een afterwork, kindernamiddag, fietstocht, BBQ, kampvuur, fuif en optreden. En dat allemaal in de mooiste groene oase van Sint-Niklaas. Alle info en prijzen via www.olmenfeesten.be",
                         new List<Moment>{
                             new Moment(new DateTime(2019,10,12), new TimeSpan(6,30,0), new TimeSpan(10,0,0)),
                             new Moment(new DateTime(2019,10,13), new TimeSpan(5,0,0), new TimeSpan(11,0,0))}, 
                         new Locatie("9100", "lepenstraat 78c", "Sint-Niklaas"), Categorie.Acoustic,
                         new Gallerij(
-                            new Fotograaf("Lisa Oliviero","profielfotolisa.png",new List<string>(){ "twitter.be/lisa23","facebook.nl/lisaolivero"}),
-                            new List<string>(){"foto1.png","foto2.png","foto3.png","foto6.png"}
+                            lisa,
+                            new List<string>(){ "kindernamiddagolmen.jpeg", "olmenfeesten3.png", "Achter-no-video.jpg", "olmenfeesten1.png",
+                                "scoutsdolmen.png", "scoutsdolmen2.png" }
                         ),
-                        new List<String>{"Twitter.be","Facebook.be"}
+                        new List<String>{"twitter.be/Olmenfeesten","Facebook.be/Scoutsdolmen"}
                         ,"profiel10.jpg","omslag10.jpg",null
                     ),
 
@@ -52,8 +57,8 @@ namespace MuziekInVlaanderenAPI.Data
                             new Moment(new DateTime(2020,1,16), new TimeSpan(6,0,0), new TimeSpan(10,30,0))}
                         , new Locatie("93200", "Werfplein", "Aalst"), Categorie.Rock,
                         new Gallerij(
-                            new Fotograaf("Robert Van Straeten","profielfotorobert.png",new List<string>(){ "twitter.be/robert","facebook.nl/roberto"}),
-                            new List<string>(){"foto1.png","foto2.png","foto3.png","foto6.png"}
+                            robert,
+                            new List<string>(){ "aalst-69.jpg", "kattepepermens.jpg", "Lavie-Ebbel-tijdens-Kattepeper-in-Aalst-Persregio-Dender.jpg"}
                         ),
                         new List<string>{"twitter.be","facebook.be"}
                         , "profiel1.jpg","omslag1.jpg",15.00
@@ -66,26 +71,33 @@ namespace MuziekInVlaanderenAPI.Data
                             new Moment(new DateTime(2019,9,21), new TimeSpan(7,0,0), new TimeSpan(10,0,0)),
                             new Moment(new DateTime(2019,9,22), new TimeSpan(7,0,0), new TimeSpan(10,0,0))}
                         , new Locatie("3060", "Lange ZoutStraat", "Bertem"), Categorie.Rock,
-                        null,null,"profiel3.jpg","omslag3.jpg",7.00
+                        new Gallerij(
+                            robert,
+                            new List<string>(){ "delikt-01.png" , "beatstreet-01.png", "Sunny-After-Moon-7.jpg"
+                                ,"le77-01.png" , "Irie-Lion-14.jpg", "sunnyaftermoon-01.png"}
+                            )
+                       ,new List<string>{ "twitter.be/voerrock","facebook.be/voerrock19" }
+                       ,"logoInkspot-web.png","omslag3.jpg",7.00
                     ),
 
                     new Evenement("D Festival ","D Festival is het jaarlijkse danstreffen waarvoor Théâtre des Tanneurs, Théâtre Marni en Le Senghor de handen in elkaar slaan. Het D-festival zet voortaan het avontuur voort in april, in het verlengde van de lentevakantie. Het spelterrein van D blijft evenwel trouw aan zichzelf: drie Brusselse cultuuroorden werken samen om, binnen het kader van een gedeelde dynamiek, ruimte te geven aan choreografen van wie het werk nauw aansluit bij wat er vandaag reilt en zeilt. Zo combineren de voorstellingen van deze editie vlijmscherpe sociale kritiek met de noodzaak om de wereld opnieuw in betovering te brengen. ",
                         new List<Moment>{
                             new Moment(new DateTime(2019,9,19), new TimeSpan(8,0,0), new TimeSpan(11,40,0))}
                         , new Locatie("9000", "Welleweg", "Gent"), Categorie.Nineties,
-                        new Gallerij(
-                            new Fotograaf("Lisa Oliviero","profielfotolisa.png",new List<string>(){ "twitter.be/lisa23","facebook.nl/lisaolivero"}),
-                            new List<string>(){"foto1.png","foto2.png","foto3.png","foto6.png"}
-                        ),null,"profiel11.jpg","omslag11.jpg",20.00
+                        null,null,"profiel11.jpg","omslag11.jpg",20.00
                     ),
 
-                    new Evenement("Slow guitar evening","Rhythm Naturals en De Centrale brengen met deze 7e editie van UAF opnieuw een gevarieerd jongerenprogramma. Op het programma staan een aantal cross-over danscreaties, de Vlaamse pre-selecties van de internationale End Of the Weak Belgium MC battle, workshops en enkele presentaties van cultureel ondernemerschap. Top of the bill is de finale van de MNM Urbanice Battle Of Talents. De 10 finalisten tonen op UAF het beste van zichzelf tijdens een live uitzending van MNM Urbanice vanuit De Centrale. ",
+                    new Evenement("Official guitar day","Rhythm Naturals en De Centrale brengen met deze 7e editie van UAF opnieuw een gevarieerd jongerenprogramma. Op het programma staan een aantal cross-over danscreaties, de Vlaamse pre-selecties van de internationale End Of the Weak Belgium MC battle, workshops en enkele presentaties van cultureel ondernemerschap. Top of the bill is de finale van de MNM Urbanice Battle Of Talents. De 10 finalisten tonen op UAF het beste van zichzelf tijdens een live uitzending van MNM Urbanice vanuit De Centrale. ",
                         new List<Moment>{
                             new Moment(new DateTime(2019,11,8), new TimeSpan(7,0,0), new TimeSpan(10,0,0)),
                             new Moment(new DateTime(2019,11,9), new TimeSpan(7,0,0), new TimeSpan(10,30,0))}
                         , new Locatie("7100", "Halve Maatweg", "Brussel"), Categorie.Acoustic,
-                        null,
-                        new List<string>{"twitter.be","facebook.be"},
+                        new Gallerij(
+                            lisa,
+                            new List<string>{ "acoustic1.jpg", "crossroads_guitar.jpg", "427540_rochehaut-panoramasurfrahan_c_wbt-sergematterne.jpg",
+                            "jarrod_dickenson_and_atkins_guitars01_website_image_golw_standard.jpg","atkin_guitars22_website_image_golw_standard.jpg"}
+                            ),
+                        new List<string>{"twitter.be/guitarday","facebook.be/guitarday19"},
                         "profiel4.jpg","omslag4.jpg",10.00
                     ),
 
@@ -98,10 +110,12 @@ namespace MuziekInVlaanderenAPI.Data
                         , new Locatie("6500", "Volksplein", "Antwerpen"), Categorie.Pop,
                         null,null,"profiel7.jpg","omslag7.jpg",null
                     ),
-                    new Evenement("Skrillex dnb dubstep",BESCHRIJVING,
+                    new Evenement("Disco REDBULL",BESCHRIJVING,
                         new List<Moment>{
                             new Moment(new DateTime(2019,10,19), new TimeSpan(9,0,0), new TimeSpan(10,0,0))}
-                        , new Locatie("9300", "Italienweg", "Aalst"), Categorie.EMD
+                        , new Locatie("9300", "Italienweg", "Aalst"), Categorie.EMD,
+                        null,null,
+                        "profiel6.jpg","omslag6.jpg",6.00
                     )
                 };
                 _dbContext.Evenementen.AddRange(lijstEvenementen);
